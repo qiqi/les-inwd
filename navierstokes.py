589a03cf1392b1d931947379bdf89fdc818b9fd1
Qiqi Wang <qiqi@mit.edu>
2017-05-13 19:45:46 -0700

    Got rid of settings

--- navierstokes.py
+++ navierstokes.py
@@ -3,10 +3,43 @@ import sys
 import time
 from numpy import *
 
-from settings import *
 from utilities import *
 from convection import *
 from pressure import *
+
+class Settings:
+    def __init__(self, x, y, z, nu, dt, tol):
+        assert x.ndim == 1 and y.ndim == 1 and z.ndim == 1
+        assert (x[1:] > x[:-1]).all()
+        assert (y[1:] > y[:-1]).all()
+        assert (z[1:] > z[:-1]).all()
+        assert nu > 0 and dt > 0 and tol > 0
+        self.x, self.y, self.z, self.nu, self.dt, self.tol \
+                = x[:,newaxis,newaxis], \
+                  y[newaxis,:,newaxis], \
+                  z[newaxis,newaxis,:], nu, dt, tol
+
+    @property
+    def nx(self): return self.x.size - 1
+    @property
+    def ny(self): return self.y.size - 1
+    @property
+    def nz(self): return self.z.size - 1
+
+    @property
+    def dx(self): return diff(self.x, axis=0)
+    @property
+    def dy(self): return diff(self.y, axis=1)
+    @property
+    def dz(self): return diff(self.z, axis=2)
+
+    @property
+    def xc(self): return (self.x[1:,:,:] + self.x[:-1,:,:]) / 2
+    @property
+    def yc(self): return (self.y[:,1:,:] + self.y[:,:-1,:]) / 2
+    @property
+    def zc(self): return (self.z[:,:,1:] + self.z[:,:,:-1]) / 2
+
 
 class NavierStokes:
     def __init__(self, xgrid, ygrid, zgrid, nu, dt, tol=1E-12,
@@ -71,6 +104,7 @@ class NavierStokes:
         t4 = time.time()
         self.f_log.write("Timing: {0:.1e} {1:.1e} {2:.1e} {3:.1e}\n".format(
                              t1-t0, t2-t1, t3-t2, t4-t3))
+
 
 if __name__ == '__main__':
     def kinetic_energy(u): return (u**2).sum(0).mean() / 2
